0f4d7e6b907d30a03742e30c736d4eb5bad2cf92
Nicolas Theodoro <nicolastheodoro@gmail.com>
2021-07-10 12:07:39 -0300

    Aula 06

--- projeto03/src/HomeScreen.js
+++ projeto03/src/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen = ({navigation}) => {
   return (
@@ -10,6 +11,42 @@ const HomeScreen = ({navigation}) => {
         onPress={() => navigation.openDrawer()}></Button>
     </View>
   );
+};
+
+const storeData = async value => {
+  try {
+    await AsyncStorage.setItem('@storage_Key', value);
+  } catch (e) {}
+};
+
+const storeObject = async value => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem('@storage_Key', jsonValue);
+  } catch (e) {}
+};
+
+const getData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('@storage_Key');
+    if (value !== null) {
+      console.log(value);
+    }
+  } catch (e) {}
+};
+
+const getObject = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@storage_Key');
+    return jsonValue !== null ? console.log(JSON.parse(jsonValue).name) : null;
+  } catch (e) {}
+};
+const deleteKey = async () => {
+  try {
+    await AsyncStorage.removeItem('@storage_Key');
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 export default HomeScreen;
